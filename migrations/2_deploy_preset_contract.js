99f7b8bfde0fd17c96fdc5994798b87f85dbb9e2
phat <phat.pham@nfq.asia>
2021-05-23 23:25:58 +0700

    chore: cleaning code

--- migrations/2_deploy_preset_contract.js
+++ migrations/2_deploy_preset_contract.js
@@ -1,6 +1,4 @@
-const ERC721 = artifacts.require('ERC721');
 const PresetERC721 = artifacts.require('ERC721PresetMinterPauserAutoId');
-
 
 module.exports = function(deployer) {
     deployer.deploy(PresetERC721, 'score', 'SCR', 'self-hosted-service');
